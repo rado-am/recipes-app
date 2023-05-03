399018b0bcbfcf2d86405ec73d6f680cbeadb26c
Mateusz Miszczak <mthw.miszczak@gmail.com>
2023-05-03 21:06:50 +0200

    PageLoader component finished, animated pan created, animated header created, animated background gradient created, folder for PageLoader scss files created, 3 files added: _PageLoaderAnimations, _PageLoaderMixins and _PageLoaderVariables

--- frontend/src/routes/components/PageLoader/PageLoader.tsx
+++ frontend/src/routes/components/PageLoader/PageLoader.tsx
@@ -2,4 +2,24 @@ import type { FC } from "react";
 
 import styles from "./PageLoader.module.scss";
 
-export const PageLoader: FC = () => <div className={styles["page-loader"]}>Loading...</div>;
+export const PageLoader: FC = () => (
+  <div className={styles["page__loader"]}>
+    <h1 className={styles["page__loader--h1"]}>Cooking in progress...</h1>
+    <div className={styles["cooking"]}>
+      <div className={styles["cooking__bubble"]} />
+      <div className={styles["cooking__bubble"]} />
+      <div className={styles["cooking__bubble"]} />
+      <div className={styles["cooking__bubble"]} />
+      <div className={styles["cooking__bubble"]} />
+      <div className={styles["cooking__area"]}>
+        <div className={styles["cooking__area--sides"]}>
+          <div className={styles["cooking__area--sides-pan"]} />
+          <div className={styles["cooking__area--sides-handle"]} />
+        </div>
+        <div className={styles["cooking__area--pancake"]}>
+          <div className={styles["cooking__area--pancake-pastry"]} />
+        </div>
+      </div>
+    </div>
+  </div>
+);
