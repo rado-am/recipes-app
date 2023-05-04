8cb98834b0f796f966210cacc8995f6ab8aedb97
Mateusz Miszczak <mthw.miszczak@gmail.com>
2023-05-04 23:47:20 +0200

    Testing font deleted, height and width removed from .page__loader, {styles['cooking']} replaced with {styles.cooking}, global file _mixins.scss created, local one deleted, breakpoints created, mixin for responsive design created, local _PageLoaderVariables.scss deleted, variables for animations moved to _PageLoaderAnimations.scss file, responsiveness fixed

--- frontend/src/routes/components/PageLoader/PageLoader.tsx
+++ frontend/src/routes/components/PageLoader/PageLoader.tsx
@@ -5,7 +5,7 @@ import styles from "./PageLoader.module.scss";
 export const PageLoader: FC = () => (
   <div className={styles["page__loader"]}>
     <h1 className={styles["page__loader--h1"]}>Cooking in progress...</h1>
-    <div className={styles["cooking"]}>
+    <div className={styles.cooking}>
       <div className={styles["cooking__bubble"]} />
       <div className={styles["cooking__bubble"]} />
       <div className={styles["cooking__bubble"]} />
