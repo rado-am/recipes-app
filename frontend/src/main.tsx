1a01e835a4d235e42b8197f263e5d4ccad13941e
rado-am <radek.luczak@icloud.com>
2023-05-06 18:31:46 +0200

    Add redux

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 
 import Router from "routes/Router";
+import store from "store/store";
 
 import "./scss/styles.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={Router} />
+    <Provider store={store}>
+      <RouterProvider router={Router} />
+    </Provider>
   </React.StrictMode>
 );
