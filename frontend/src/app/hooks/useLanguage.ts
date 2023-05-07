a36d42b38bab68e109a21bb8a767128c1147bb04
NorGoz <norbert1303@wp.pl>
2023-05-07 19:53:01 +0200

    crate useLanguage

--- frontend/src/app/hooks/useLanguage.ts
+++ frontend/src/app/hooks/useLanguage.ts
@@ -2,10 +2,14 @@ import { setLanguage } from "app/appSlice";
 import { Language } from "app/enums";
 import { useAppDispatch } from "store/hooks";
 
-const useLanguage = () => {
+interface Props {
+  language: Language;
+}
+
+const useLanguage = ({ language }: Props) => {
   const dispatch = useAppDispatch();
 
-  dispatch(setLanguage(Language.PL));
+  dispatch(setLanguage(language));
 };
 
 export default useLanguage;
